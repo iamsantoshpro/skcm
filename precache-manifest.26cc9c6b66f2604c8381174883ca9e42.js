f4d60bffcdba5ef7d50b0e84d408a31710afdee3
iamsantoshpro <santosh.bm302@gmail.com>
2021-01-05 13:37:19 +0000

    Initial commit

---
+++ precache-manifest.26cc9c6b66f2604c8381174883ca9e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e089d00fc175b4f2ea5dc46e222d9d2b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3babcbe7658fe43e72a2",
+    "url": "/portfolio/static/css/main.224d39c2.chunk.css"
+  },
+  {
+    "revision": "3ebcf91e668d3531a2c8",
+    "url": "/portfolio/static/js/2.5b9a5aed.chunk.js"
+  },
+  {
+    "revision": "66dda2b29fcaf6c949090d5c91de6386",
+    "url": "/portfolio/static/js/2.5b9a5aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3babcbe7658fe43e72a2",
+    "url": "/portfolio/static/js/main.9e98fe27.chunk.js"
+  },
+  {
+    "revision": "56275c1fa6be999e4306",
+    "url": "/portfolio/static/js/runtime-main.4c5db381.js"
+  }
+]);
